76ec09f26b165c0acc98e46b0e68e0cae66749c6
yiran.huang <yiran.huang@tum.de>
2025-11-04 20:28:13 +0100

    add rule and real class names for open mi

--- utils/ICL_utils.py
+++ utils/ICL_utils.py
@@ -25,7 +25,9 @@ def select_demonstration(support_meta, n_shot, dataset, query=None):
                 support = {
                     'image': [query['support'][key]['images'][i]], 
                     'answer': answers[key],
-                    'question': "This is a"
+                    'question': "This is a ",
+                    'real_name': query['support'][key]['real_name'],
+                    'mapping': f"{query['support'][key]['real_name']} maps to {answers[key]}"
                 }
                 n_shot_support.append(support)
     
@@ -35,44 +37,6 @@ def select_demonstration(support_meta, n_shot, dataset, query=None):
         for i in range(n_shot):
             n_shot_support.append(n_shot_support_raw[i]['same'])
             n_shot_support.append(n_shot_support_raw[i]['diff'])
-    
-    elif dataset == 'open_t2i_mi' or dataset == 'fast_attr_t2i' or dataset == 'fast_count_t2i':
-        query_class = query['task_label']
-        other_class = random.choice([cls for cls in query['classes'] if cls != query_class])
-        order_keys = [query_class, other_class] if random.choice([True, False]) else [other_class, query_class]
-        answers = {query_class: query_class, other_class: other_class}
-        n_shot_support = []
-        for i in range(n_shot):
-            for key in order_keys:
-                # For each key, add one shot
-                if dataset == 'open_t2i_mi':
-                    support = {
-                        'image': query['support'][key]['images'][i], 
-                        'question': f'Generate a {key}'
-                    }
-                elif dataset == 'fast_attr_t2i':
-                    support = {
-                        'image': query['support'][key]['images'][i], 
-                        'question': f"Generate a {query['support'][key]['captions'][i]}"
-                    }
-                elif dataset == 'fast_count_t2i':
-                    support = {
-                        'image': query['support'][key]['images'][i], 
-                        'question': f"Generate {query['support'][key]['captions'][i]}"
-                    }
-                else:
-                    support = {
-                        'answer': query['support'][key]['images'][i], 
-                        'question': f'Generate a {key}'
-                    }
-                n_shot_support.append(support)
-    elif dataset == 'cobsat':
-        latent_var = query['latent']
-        latent = query[latent_var]
-        task = query['task']
-        # get support set with same latents
-        n_shot_support = [x for x in support_meta if (x[latent_var] == latent and x['latent'] == latent_var and x['task'] == task)]
-        n_shot_support = copy.deepcopy(random.sample(n_shot_support, n_shot))
     else:
         n_shot_support = random.sample(support_meta, n_shot)
     return n_shot_support
@@ -80,9 +44,9 @@ def select_demonstration(support_meta, n_shot, dataset, query=None):
 def get_task_instruction(args):
     dataset = args.dataset
     description = args.task_description
+    rule_instr = None
     if description == 'nothing':
         instr = ''
-        return instr
     
     if dataset == 'textocr':
         if description == 'concise':
@@ -104,6 +68,9 @@ def get_task_instruction(args):
             instr = 'Answer the question with a single word or phase.'
         elif description == 'detailed':
             instr = "Induce the concept from the in-context examples. Answer the question with a single word or phase."
+        elif description == 'rule':
+            instr = "You will see images labeled with invented names from this set: 'blicket', 'dax', 'shously', 'perpo', or 'slation'. Each name corresponds to a specific object category. Learn which name matches which object type from the examples. Then, identify the object in the test image and respond with its corresponding invented name. Answer with only that single word."
+        rule_instr = "You will be shown images of animals. Your task is to label each image using exactly one invented category name from the set [blicket, dax, shously, perpo, slation]. Each invented name maps to exactly one real-world animal category. Known mappings: {}. If you are uncertain, make your best guess using only the allowed labels. Respond with exactly one label from [blicket, dax, shously, perpo, slation]"
     elif dataset == 'clevr':
         if description == 'concise':
             instr = 'Find objects of the given type, induce what operation to use and calculate the result.'
@@ -114,20 +81,8 @@ def get_task_instruction(args):
             instr = 'Determine the output for the new pair of images.'
         elif description == 'detailed':
             instr = 'According to the few-shot examples, induce what operation to do and determine the output for the two new images. Answer with "yes" or "no".'
-    
-    # t2i
-    elif dataset == 'cobsat':
-        if description == 'concise':
-            instr = 'Generate the next image based on the latent object or attribute from the few-shot examples.'
-        elif description == 'detailed':
-            instr = 'Based on the sequence, infer the latent object or attribute. Generate the image of the inferred object or attribute combined with the given text.'
-    elif dataset == 'open_t2i_mi':
-        if description == 'concise':
-            instr = 'Generate the image of the given class.'
-        elif description == 'detailed':
-            instr = 'Based on the few-shot examples, induce the concept and generate the image of the given class.'
-    
-    return instr
+
+    return rule_instr,instr
 
 def format_answer(answer, dataset, query=None):
     if dataset in ['operator_induction', "clevr", 'operator_induction_interleaved']:
